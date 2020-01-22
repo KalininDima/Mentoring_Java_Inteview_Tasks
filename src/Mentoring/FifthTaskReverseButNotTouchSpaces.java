cee7568d6f986315581e73414d2e3a0b9dfedee2
Dima Kalinin <dmitriy_kalinin@vseyfe.com>
2020-01-22 13:45:30 -0500

    Mentoring code 2

--- src/Mentoring/FifthTaskReverseButNotTouchSpaces.java
+++ src/Mentoring/FifthTaskReverseButNotTouchSpaces.java
@@ -6,25 +6,60 @@ import java.util.Arrays;
 public class FifthTaskReverseButNotTouchSpaces {
 	public static void ReverseNotTouchSpaces(String str) {
 
+		//create the Array list with Integer data
 		ArrayList<Integer> listofspaces = new ArrayList<>();
+		
+		//create the for loop with condition the length of main String str
+		
 		for (int i = 0; i < str.length() - 1; i++) {
+			
+			//create if statement and check that if one of the index equal space, go inside
 			if ((""+str.charAt(i)).equals(" ")) {
+				
+				//And save into the list that index which have space from main String str
 				listofspaces.add(i);
 			}
 		}
 		
+		//then reassign str variable using removeAll method, and remove all spaces from that String variable
 		str=str.replaceAll(" ","");
+		
+		//Create new String result
 		String result="";
+		
+		//Create for loop with int i as initializer where value is length of main String str -1
+		//and condition i must be high or equal zero, and iterator i--, 
+		//that means that we are going from end of the value to the begigining
 		for (int i = str.length() - 1; i >= 0; i--) {
+			
+			//Create inside that loop another one, initializer int l will be 0, condition l should be less then length of 
+			//ArrayList where we saved indexes with spaces from our main String
 			for (int l = 0; l < listofspaces.size(); l++) {
+				
+				//the inside the loop create the if statement  with condition
+				//if array list has the value(that values equals to the indexes of all spaces from our main String str)
+				//so if one of that value equal to to the length of our new String result, go inside the if statement
 				if (listofspaces.contains(result.length())) {
+					
+					//and add the " " to the new String variable result
 					result += " ";
-				} else {
+				
+				}
+				//if our if statement false go inside the else statement 
+				else {
+					
+					//and the letter from our main String str to the new String result
+					//Reminder: we are going from the end of the main String, and we are did remove all spaces from main String
+					//but we know where were spaces in that String , because we saved their indexes in the our ArrayList<Integer>.
 					result += str.charAt(i);
+					
+					//then break, exit from current loop and come back to first loop
 					break;
 				}
 			}
 		}
+		
+		//Done, print the result
 		System.out.println(result);
 
 		
--- src/Mentoring/FourthTaskArmstrong.java
+++ src/Mentoring/FourthTaskArmstrong.java
@@ -6,31 +6,55 @@ public class FourthTaskArmstrong {
 	/*
 	 * Armstrong Number in Java: A positive number is called armstrong number if it
 	 * is equal to the sum of cubes of its digits for example: 153 =
-	 * (1*1*1)+(5*5*5)+(3*3*3) Where: (1*1*1)=1 (5*5*5)=125 (3*3*3)=27
+	 * (1*1*1)+(5*5*5)+(3*3*3)
+	 * Where: (1*1*1)=1 (5*5*5)=125 (3*3*3)=27
 	 * So:1+125+27=153
 	 */
 
+	//create the method with int parameters
 	public static void ArmstrongFormula(int Armstrong) {
+		
+		//assign the armstrong value to String
 		String str = "" + Armstrong;
+		
+		//create int result, we will save the our result
 		int result = 0;
+		
+		//Split the string which has a value of Armstrong to String Array
 		String[] splited = str.split("");
+		
+		//Create the int Array with size of string where we saved value of Armstrong
 		int[] i = new int[str.length()];
 
+		//creating for loop with condition j less the length of str String
 		for (int j = 0; j < str.length(); j++) {
+			
+			//then we are taking int array and assign to each index of that array, each index from string array, 
+			//to converting from string to int helps us to converting string to int by parseInt method. that Un-boxing.
 			i[j] = Integer.parseInt(splited[j]);
 		}
 
+		//printing that int array
 		System.out.println(Arrays.toString(i));
 
+		//create for each int loop using int array
 		for (int each : i) {
+			
+			//addition to int variable methematic result, each - its value of each index our int array
 			result += each * each * each;
+			
+			//printing result of 3 times multiplication
 			System.out.println(each * each * each + "\n");
 		}
-
+		//creating if statement with condition value with int result is equal with value int Armstrong
 		if (result == Armstrong) {
+			
+			//if yes, printing results of variables and true 
 			System.out.println(
 			"Thats Armstrong numbers\n" + "Actual result: " + result + "\n" + "Excpected result: " + Armstrong);
-		} else {
+		}
+		//if not , print result of variables and false
+		else {
 			System.out.println("Thats not Armstrong numbers\n" + "Actual result: " + result + "\n"
 					+ "Excpected result: " + Armstrong);
 		}
